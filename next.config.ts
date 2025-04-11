f6f8e155f9695757d4b9490a119780d2213b43e4
rraushan1205 <raushanraj1205@gmail.com>
2025-04-12 03:34:23 +0530

    prisma error resolved

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+const { execSync } = require("child_process");
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output:"export",
 };
 
 export default nextConfig;
+// Ensure Prisma Client is generated during the build process
+
+if (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+  execSync("npx prisma generate", { stdio: "inherit" });
+}
