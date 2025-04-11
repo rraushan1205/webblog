393dac77f0744f30c9f2c7bc7f60fb725c9742a6
rraushan1205 <raushanraj1205@gmail.com>
2025-04-12 04:43:06 +0530

    changes in signup route

--- app/api/user/signup/route.tsx
+++ app/api/user/signup/route.tsx
@@ -7,12 +7,19 @@ export async function POST(request: Request) {
     try {
         const body = await request.json();
         const { name, email, password } = body;
-
         if (!name || !email || !password) {
             return NextResponse.json(
                 { error: 'Missing required fields' },
                 { status: 400 }
             );}
+        // Check if user already exists
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
+        if (existingUser) {
+            return NextResponse.json({error: 'User already exists'}, { status: 409 });}
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await prisma.user.create({
             data: {
@@ -21,6 +28,12 @@ export async function POST(request: Request) {
                 password: hashedPassword,
             },
         });
+        if(!user){
+            return NextResponse.json(
+                { error: 'User creation failed' },
+                { status: 201 }
+            );
+        }
     
         return NextResponse.json({ message: 'User created successfully', user}, { status: 201 });
     } catch (error) {
